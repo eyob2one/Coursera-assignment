bdaccb4360dc503b295c5923a2880e88d6e0abec
Eyob Mengistu <eyob2one@gmail.com>
2022-08-06 14:37:47 -0700

    Another approach.

--- module4-solution/harder/script.js
+++ module4-solution/harder/script.js
@@ -1,15 +1,25 @@
 
 (function() {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-  // for (let i = 0; i < names.length; i++) {
-  // let firstLetter = names[i].charAt(0).toLowerCase();
-  names.forEach(name => {
+// Using the forEach Method
+  //  names.forEach(name => {
+  //   let firstLetter = name.charAt(0).toLowerCase();
+  //   if (firstLetter === 'j') {
+  //     byeSpeaker.speak(name)
+  //   } else {
+  //     helloSpeaker.speak(name)
+  //   }
+  //   })
+  
+// Using the for of Loop
+  for(let name of names) {
     let firstLetter = name.charAt(0).toLowerCase();
     if (firstLetter === 'j') {
       byeSpeaker.speak(name)
-    } else {
+    }
+    else {
       helloSpeaker.speak(name)
     }
-    })
+  }
   }
 )();
